0af9774f6c8daa50ed279a1d198840e267f6d70e
NiklasFerling <niklas.ferling@gmail.com>
2024-02-27 16:06:53 +0100

    Makes create func use authfetch

--- src/js/api/listings/create.mjs
+++ src/js/api/listings/create.mjs
@@ -1,7 +1,10 @@
+import { load } from "../../storage/load.mjs"
 import * as constants from "/src/js/api/constants.mjs"
 const action = "/listings"
 
-export async function createPost(id) {
+export async function createPost(postData) {
     const createListingURL = constants.API_AUCTION_URL + action
-    const response = await fetch(createListingURL)
+    const response = await fetch(createListingURL, {
+        body: JSON.stringify(postData)
+    })
 }
