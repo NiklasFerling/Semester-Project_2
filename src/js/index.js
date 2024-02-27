745df0e909a2198c491a5c4ce4cfa28dda1c437d
NiklasFerling <niklas.ferling@gmail.com>
2024-02-27 15:25:43 +0100

    Imports listing functions to index

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 import { setRegisterFormListener } from "./api/handlers/register.mjs";
 import { setLoginFormListener } from "./api/handlers/login.mjs";
-import * as constants from "./api/constants.mjs"
 import storage from "./storage/index.mjs";
+
+import * as constants from "./api/constants.mjs"
+import * as listings from "./api/listings/index.mjs"
 
 setRegisterFormListener()
 setLoginFormListener()
+
+console.log(listings);
