c1df3c5e64a0b9d82821ae54ae189fd25890b1d2
NiklasFerling <niklas.ferling@gmail.com>
2024-02-28 13:47:12 +0100

    Adds get functions

--- src/js/api/listings/get.mjs
+++ src/js/api/listings/get.mjs
@@ -1,6 +1,21 @@
+import { authFetch } from "../auth/fetch.mjs"
+import * as constants from "/src/js/api/constants.mjs"
+
+const action = "/listings"
+
 export async function getListings() {
- //   
+    const getListingURL = constants.API_AUCTION_URL + action
+    const response = await authFetch(getListingURL)
+    const result = await response.json()
+    console.log(result);
 }
+
 export async function getListing(id) {
-    //
+    if (!id) {
+        throw new Error("id is required")
+    }
+    const getListingURL = constants.API_AUCTION_URL + action + "/" + id
+    const response = await authFetch(getListingURL)
+    const result = await response.json()
+    console.log(result);
 }
