fa01df60fe1c36d493daba5c8b91c84accb60d93
NiklasFerling <niklas.ferling@gmail.com>
2024-02-29 16:03:11 +0100

    Adds card template and render

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,23 @@
 import { setRegisterFormListener } from "./api/handlers/register.mjs";
 import { setLoginFormListener } from "./api/handlers/login.mjs";
-import { profileBtn, renderProfileBtn } from "./templates/profileBtn.mjs";
-import { card, renderCard } from "./templates/card.mjs";
 
 import storage from "./storage/index.mjs";
 
+import * as template from "./templates/index.mjs"
 import * as constants from "./api/constants.mjs"
 import * as listings from "./api/listings/index.mjs"
 
 setRegisterFormListener()
 setLoginFormListener()
+template.renderProfileBtn()
 
-card(listings.getListings())
-renderProfileBtn();
-renderCard()
+
+async function testTemplate() {
+    const array = await listings.getListings()
+    const container = document.querySelector("#item-container")
+    array.forEach(element => {
+        console.log(element);
+        template.renderCard(element, container)
+    });
+}
+testTemplate()
--- src/js/templates/card.mjs
+++ src/js/templates/card.mjs
@@ -1,11 +1,36 @@
 export function card(listing) {
-    console.log(listing);
     const card = document.createElement("a")
-    const img = document.createElement("img")
-    // img.setAttribute("src", listing.media[0])
     card.classList.add("card", "col-lg-2", "col-md-3", "col-sm-6", "p-0", "rounded-0", "mb-3", "text-decoration-none")
+    card.setAttribute("href", `/listing/?${listing.id}`)
+    const img = document.createElement("img")
+    img.setAttribute("src", listing.media[0])
+    img.setAttribute("style", "height: 220px")
+    const cardBody = document.createElement("div")
+    cardBody.classList.add("card-body")
+    const cardTitle = document.createElement("h5")
+    cardTitle.classList.add("card-title")
+    cardTitle.innerText = listing.title
+
+    card.append(img)
+    card.append(cardBody)
+    cardBody.append(cardTitle)
+    if (listing.bids.length > 0) {
+        const biddersName = document.createElement("p")
+        const bid = document.createElement("p")
+        biddersName.innerText = "Current Bidder: " + listing.bids[0].bidderName
+        
+        cardBody.append(biddersName)
+        cardBody.append(bid)
+    } else {
+        const biddersName = document.createElement("p")
+        biddersName.innerText = "(no current bids)"
+        
+        cardBody.append(biddersName)
+
+    }
+    return card
 }
 
-export function renderCard() {
-    const container = document.querySelector("#item-container")
+export function renderCard(listing, container) {
+    container.append(card(listing))
 }
