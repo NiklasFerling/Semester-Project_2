438c4e57dd4909918f2dddb5360d7316a582241d
NiklasFerling <niklas.ferling@gmail.com>
2024-03-02 00:51:14 +0100

    Place bid function part 1

--- src/js/api/auth/fetch.mjs
+++ src/js/api/auth/fetch.mjs
@@ -2,14 +2,14 @@ import { load } from "../../storage/load.mjs"
 
 export function headers() {
     const token = load("token")
-    return {
+    console.log(
+     {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
-    }
+    })
 }
 export async function authFetch(url, options = {}) {
     return fetch(url, {
-        ...options,
-        headers: headers()
+        ...options
     })
 }
